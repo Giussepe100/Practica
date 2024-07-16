54ea12e70ba3564b9cee648eaf1bc3bd1d8040eb
Alexander <cresistem99@gmail.com>
2024-07-16 18:43:12 -0500

    Se crea interfaz gráfica  de recomendacion.html

--- app/app.py
+++ app/app.py
@@ -122,7 +122,7 @@ def MostrarDirectoresXTipoPelicula(tipoPelicula):
         return ResultadoPelicula
     else:
         return ResultadoPelicula[:5]
-#wsddsd
+#wsddsdd
 def get_pelicula_trabajado_director(nombredirector):
     #filtrar el dataframe por el nombre del director 
     global org_movies
