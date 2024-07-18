ed85f038a9a7da097e30e6505e39cbf56a43355b
Alexander <cresistem99@gmail.com>
2024-07-18 09:49:27 -0500

    Se creo el archivo heder.html barra de navegación

--- app/app.py
+++ app/app.py
@@ -122,7 +122,7 @@ def MostrarDirectoresXTipoPelicula(tipoPelicula):
         return ResultadoPelicula
     else:
         return ResultadoPelicula[:5]
-#wdd dddsf
+#wdd dddsf dd
 def get_pelicula_trabajado_director(nombredirector):
     #filtrar el dataframe por el nombre del director 
     global org_movies
